8bbafd8a54a9b09fdbd0cf014c34260f8e2f3ef8
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2022-09-10 14:13:31 -0400

    added darkmode toggler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './assets/scss/style.scss';
 import Country from './components/country/Country';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 function App() {
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleDarkMode = () => setDarkMode(prevMode => !prevMode)
+
     return (
-        <div className="app">
-            <Header />
+        <div className={`app ${darkMode ? 'app--dark' : 'app--light'}`}>
+            <Header toggleDarkMode={toggleDarkMode} />
             <div className="app__content">
                 <Routes>
                     <Route path="/" element={<Home />} />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,14 @@
-const Header = () => {
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = ({toggleDarkMode}) => {
     return (
         <div className="header">
             <div className="header__container container">
                 <h1 className="header__title">Where in the world?</h1>
-                <button className="header__toggler">dark mode</button>
+                <button onClick={toggleDarkMode} className="header__toggler">
+                    <FontAwesomeIcon icon={faMoon} /> dark mode
+                </button>
             </div>
         </div>
     );
