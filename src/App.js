5e07938075b5fba0d1364576b3ca76855f086ead
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2022-09-10 11:09:21 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './assets/scss/style.scss';
+import Header from './components/header/Header';
 function App() {
     return (
-        <div className="App">
-            hi
+        <div className="app">
+            <Header />
+            <div className="app__content">
+
+            </div>
         </div>
     );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__container container">
+                <h1 className="header__title">Where in the world?</h1>
+                <button className="header__toggler">dark mode</button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
